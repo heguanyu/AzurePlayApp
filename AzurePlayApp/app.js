3eb876314d309f2ab811a76ea289dd98dd9430e9
Guanyu He <ghe@linkedin.com>
2022-10-03 14:40:36 -0700

    Clear

--- AzurePlayApp/app.js
+++ AzurePlayApp/app.js
@@ -4,23 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+var port = 8080;
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(process.cwd()+"/webui/build/"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', (req,res) => {
+  res.sendFile(process.cwd()+"/my-app/build/index.html");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +31,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on the port::${port}`);
 });
 
 module.exports = app;
